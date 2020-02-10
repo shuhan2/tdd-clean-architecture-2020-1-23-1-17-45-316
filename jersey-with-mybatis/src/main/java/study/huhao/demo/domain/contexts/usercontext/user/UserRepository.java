f8df4e5f3a4fb477f0c93fc697dd6636f9d7810b
qianwu2 <qianwu2@ap.xom.com>
2020-02-10 15:54:37 +0800

    user [qian] add CRUD for user

--- jersey-with-mybatis/src/main/java/study/huhao/demo/domain/contexts/usercontext/user/UserRepository.java
+++ jersey-with-mybatis/src/main/java/study/huhao/demo/domain/contexts/usercontext/user/UserRepository.java
@@ -1,6 +1,13 @@
 package study.huhao.demo.domain.contexts.usercontext.user;
 
+import java.util.Optional;
+import java.util.UUID;
 import study.huhao.demo.domain.core.concepts.Repository;
 
 public interface UserRepository extends Repository {
+  void save(User user);
+  Optional<User> findById(UUID id);
+  boolean existsById(UUID id);
+  void deleteById(UUID id);
+
 }
